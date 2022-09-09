ccdee484c3cdde246acc7af7e0e8de0316c46e22
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-10 00:47:36 +0600

    primitive and non-primitive types diff checked

--- module39/eight.js
+++ module39/eight.js
@@ -1,4 +1,4 @@
-function scopeTest ( a, b )
+function scopeTest ( a, b, c )
 {
     // console.log( arguments )
     // console.log( arguments[ 4 ] )
@@ -11,4 +11,32 @@ function scopeTest ( a, b )
 }
 // this arguments is different from inside funtion arguments
 // console.log( arguments ) 
-console.log( scopeTest( 1, 2, 3, 4, 5, 6, 7 ) )
+// console.log( scopeTest( 1, 2, 3, 4, 5, 6, 7 ) )
+// console.log( scopeTest.length )
+
+
+
+
+
+
+// part two
+let a = 2
+let b = 4
+function sumTwo ( m, n )
+{
+    // m = 45
+    return m + n
+}
+// console.log( a )
+// console.log( sumTwo( a, b ) )
+
+
+let objOne = { name: 'sakib', age: 21 }
+function nameChange ( one )
+{
+    one.name = 'jhon'
+    one.age = 22
+}
+console.log( objOne )
+nameChange( objOne )
+console.log( objOne )
