6f662dc8916b6e36ef8db8171ff0ad9043f86192
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-10 01:42:08 +0600

    lecture one added

---
+++ module39/one.js
@@ -0,0 +1,43 @@
+// part one 
+// int n = 4
+// float m =4
+// boolean b = true
+
+// js is a dynamic type language
+var a = 41
+let b = 46
+const m = 45
+
+
+
+
+
+// part two
+// primitive type
+var num = 2
+let name = 'sakib'
+const check = true
+
+// non-primitive type
+var ages = [ 1, 2, 3, 4, 5 ]
+let object = {
+    name: 'sakib'
+}
+
+
+
+
+
+
+// part three
+let x = 4
+let y = x
+// console.log( y, x )
+y = 5
+// console.log( y )
+
+let objOne = { name: 'sakib' }
+let objTwo = objOne
+objTwo.name = 'haaland'
+objTwo = { age: 23 }
+console.log( objOne, objTwo )
