d0f86379bddfe0cb5d6f1c87153b62cd556d1e68
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-10 02:43:21 +0600

    lecture four added(coercion and comparing === vs ==)

---
+++ module39/four.js
@@ -0,0 +1,23 @@
+// let a = 2
+// let b = '2'
+// let a = 1
+// let b = true
+// let a = 0
+// let b = false
+// let a = '0'
+// let b = false
+// let a = [ 1 ]
+// let b = [ 1 ]
+// let a = { name: 'sakib' }
+// let b = { name: 'sakib' }
+if ( a == b )
+{
+    console.log( true )
+}
+else
+{
+    console.log( false )
+}
+
+// this is called type coercion or casting or conversion
+// i have to learn about how to compare between two arrys or two objects
