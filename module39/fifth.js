d85f2d77127dea7082541591eaa74ce13cc8d016
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-10 03:10:49 +0600

    added lecture four(var hoisting)

---
+++ module39/fifth.js
@@ -0,0 +1,56 @@
+function oneL ( a, b )
+{
+    const sum = a + b
+    console.log( sum )
+
+}
+// oneL( 2, 2 )
+// console.log( a )
+// console.log( sum )
+
+
+function two ( a )
+{
+    console.log( 4 )
+    if ( a > 5 )
+    {
+        let b = 4
+        console.log( 9 )
+    }
+    console.log( b )
+}
+// two( 5 )
+
+function three ( a )
+{
+    if ( a < 5 )
+    {
+        var b = 5
+        console.log( b )
+    }
+    else
+    {
+        var c = 9
+    }
+    console.log( c )
+}
+// three( 0 )
+
+
+// name()
+// function name ()
+// {
+//     console.log( 'dhaka' )
+// }
+
+// nameMy()
+// var nameMy = function ()
+// {
+//     console.log( 'hello' )
+// }
+
+namey()
+const namey = function ()
+{
+    console.log( 'hello' )
+}
