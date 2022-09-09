642015d6132deffb149432b40c0eac8b6819c3d0
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-10 02:24:25 +0600

    check truthy falsy

---
+++ module39/three.js
@@ -0,0 +1,23 @@
+// let num = undefined
+// let num = 0
+// let num = false
+// let num = ''
+
+// let num = -1
+// let num = 2
+// let num = '..'
+// let num = ' '
+// let num = '0'
+// let num = 'false'
+// let num = `null`
+
+// var num = false (!num)
+// let num = true (!!num)
+if ( !!num )
+{
+    console.log( 'truthy' )
+}
+else
+{
+    console.log( 'falsy' )
+}
