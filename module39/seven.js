30d5086284a9e8eff9f376b94366e48f311ac9f0
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-13 19:49:21 +0600

    all other module added

--- module39/seven.js
+++ module39/seven.js
@@ -6,4 +6,12 @@ function anotherGreet ( name )
 {
     console.log( `hello my dear ${ name }` )
 }
-myGreetings( anotherGreet, 'mhj' )
+// myGreetings( anotherGreet, 'mhj' )
+
+const b = 6
+// b = 6
+// console.log( b );
+
+const adventurer = { name: 'Alice', cat: { name: 'Lucy' } };
+const ratName = adventurer?.rat?.name;
+console.log( ratName )
